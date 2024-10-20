075b6b2acefe97e47af83cba686fad19a77eecd1
dev.tronghieu <dev.ngtronghieu@gmail.com>
2024-10-20 19:18:20 +0700

    chore: setup project

---
+++ lua/config/options.lua
@@ -0,0 +1,4 @@
+local opt = vim.opt
+
+opt.mouse = "a" -- Enable mouse mode
+opt.number = true
---
+++ lua/plugins/whichkey.lua
@@ -0,0 +1,14 @@
+return {
+{
+  "folke/which-key.nvim",
+  event = "VeryLazy",
+  opts = {
+    -- your configuration comes here
+    -- or leave it empty to use the default settings
+    -- refer to the configuration section below
+  },
+  keys = {
+	{ "<leader>e", "<cmd>Neotree toggle<cr>", desc = "NeoTree" },
+  },
+}
+}
