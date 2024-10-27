895db3ea6b3e6fa2284b2b4fcf14718da0d9bee4
dev.tronghieu <dev.ngtronghieu@gmail.com>
2024-10-27 14:36:05 +0700

    chore: bufferline

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -3,4 +3,6 @@ local opt = vim.opt
 opt.mouse = "a" -- Enable mouse mode
 opt.number = true
 
+opt.termguicolors = true
+
 opt.clipboard:append("unnamedplus") -- Use system clipboard
--- lua/plugins/whichkey.lua
+++ lua/plugins/whichkey.lua
@@ -18,9 +18,22 @@ return {
       { "<leader>e", "<cmd>Neotree toggle<cr>", desc = "NeoTree" },
 
       -- Telescope
-      { "<leader>f", group = "Telescope" },
+      { "<leader>f", desc = "Telescope", group = "Telescope" },
       { "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "Find Files" },
       { "<leader>fg", "<cmd>Telescope live_grep<cr>", desc = "Search words" },
+
+      -- Buffer
+      { "<leader>,", "<cmd>BufferLineCyclePrev<cr>", desc = "Prev buffer" },
+      { "<leader>.", "<cmd>BufferLineCycleNext<cr>", desc = "Next buffer" },
+      {
+        "<leader>q",
+        function()
+          local current = vim.api.nvim_get_current_buf()
+          vim.cmd("BufferLineCyclePrev")
+          vim.cmd("bdelete " .. current)
+        end,
+        desc = "Close buffer and go to prev buffer",
+      },
     },
   },
 }
