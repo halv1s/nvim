8004a8e19ace6c83a87d44d66fc51a7f571aa3a5
dev.tronghieu <dev.ngtronghieu@gmail.com>
2024-10-28 18:05:50 +0700

    chore: trouble

--- lua/plugins/whichkey.lua
+++ lua/plugins/whichkey.lua
@@ -58,6 +58,28 @@ return {
 
       -- Float Term
       { "<leader>t", "<cmd>FloatermToggle<cr>", desc = "Floaterm" },
+
+      -- Trouble
+      {
+        "<leader>xx",
+        "<cmd>Trouble diagnostics toggle<cr>",
+        desc = "Diagnostics",
+      },
+      {
+        "<leader>xX",
+        "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+        desc = "Buffer Diagnostics",
+      },
+      {
+        "<leader>xL",
+        "<cmd>Trouble loclist toggle<cr>",
+        desc = "Location List",
+      },
+      {
+        "<leader>xQ",
+        "<cmd>Trouble qflist toggle<cr>",
+        desc = "Quickfix List",
+      },
     },
   },
 }
