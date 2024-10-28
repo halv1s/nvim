d3bc431f08945b8b824edc7599f2d756fdd07cca
dev.tronghieu <dev.ngtronghieu@gmail.com>
2024-10-28 17:16:41 +0700

    chore: remove unused lsp

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -20,11 +20,6 @@ return {
       require("mason-lspconfig").setup({
         ensure_installed = {
           "ts_ls",
-          "html",
-          "cssls",
-          "tailwindcss",
-
-          "graphql",
 
           "lua_ls",
         },
@@ -37,9 +32,32 @@ return {
     config = function()
       local lspconfig = require("lspconfig")
 
-      lspconfig.ts_ls.setup({})
+      vim.diagnostic.config({
+        virtual_text = true,
+        signs = false,
+        update_in_insert = false,
+        underline = true,
+        severity_sort = true,
+      })
 
-      lspconfig.lua_ls.setup({})
+      local on_attach = function(_, bufnr)
+        local opts = { noremap = true, silent = true, buffer = bufnr }
+
+        vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+        vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
+        vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
+        vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+        vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
+        vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
+      end
+
+      lspconfig.ts_ls.setup({
+        on_attach = on_attach,
+      })
+
+      lspconfig.lua_ls.setup({
+        on_attach = on_attach,
+      })
     end,
   },
 }
